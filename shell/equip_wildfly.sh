75fa4f6458f87b3c47b274113c407e19adbcfe34
egov-systems <egov-systems@egovernments.org>
2015-09-26 12:51:40 +0530

    Update equip_wildfly.sh

--- shell/equip_wildfly.sh
+++ shell/equip_wildfly.sh
@@ -2,5 +2,5 @@
 
 wget http://downloads.egovernments.org/wildfly-9.0.0.Final.zip
 sudo unzip wildfly-9.0.0.Final.zip -d /opt
-sudo ln -s /opt/wildfly-9.0.0.Final.zip /opt/wildfly
+sudo ln -s /opt/wildfly-9.0.0.Final /opt/wildfly
 rm wildfly-9.0.0.Final.zip
